bd082358efee8fd830c4d94575c8420154eed1ba
Gordon Soutter <gordon.soutter@outlook.com>
2020-04-15 17:37:13 +0100

    adds isogram finder

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,15 @@
 const IsogramFinder = function (word) {
+  this.word = word.toUpperCase().split('')
 
 }
 
 IsogramFinder.prototype.isIsogram = function () {
 
+  unique_letters = Array.from(new Set(this.word))
+  if (this.word.length === unique_letters.length) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = IsogramFinder;
