23bb0b2049015ee3c86ce82ebaf8575388198d41
Gordon Soutter <gordon.soutter@outlook.com>
2020-04-15 18:13:02 +0100

    adds anagram finder.

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,47 @@
 const AnagramFinder = function (word) {
+  this.wordFull = word.toUpperCase()
+
+  this.word = word.toUpperCase().split('')
+
 
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
+  let isAnagram;
+  let anagramWords = []
+  for (let oWord of otherWords){
 
+    isAnagram = true;
+
+    if (oWord.toUpperCase() === this.wordFull){
+      isAnagram = false;
+    };
+
+    if (oWord.length === 0){
+      isAnagram = false;
+    };
+
+    let oWordSplit = oWord.toUpperCase().split('');
+
+
+    for (letter of oWordSplit){
+
+      let oLetterNum = oWordSplit.filter(let => let == letter);
+      let letterNum = this.word.filter(let => let == letter);
+
+      if (oLetterNum.length === letterNum.length){
+        continue;
+      }else{
+        isAnagram = false;
+      }
+    }
+    if (isAnagram === true){
+      anagramWords.push(oWord)
+    }
+
+
+  }
+  return anagramWords;
 }
 
 module.exports = AnagramFinder;
