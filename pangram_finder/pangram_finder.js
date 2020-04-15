40360c8886e8b17132431cda20585ee117fe1a04
Gordon Soutter <gordon.soutter@outlook.com>
2020-04-15 17:28:59 +0100

    adds panaggram finder

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,18 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase.toLowerCase().split('')
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+if (this.phrase.length > 0) {
+  for (let letter of this.alphabet) {
+    if (this.phrase.includes(letter) === false)  {
+      return false;
+    }
+  }
+  return true
+}
+return false
 }
 
 module.exports = PangramFinder;
